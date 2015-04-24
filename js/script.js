1f3154106d11ef7733bdb70942467bf5523c2007
113210444 WENDLEY PAULO DE FRANCA <wendleypf@lcc2-03.lcc.ufcg.edu.br>
2015-04-24 14:32:07 -0300

    pagina

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    /*
+     * Adcionar Elemento
+     */
+    $(".form-horizontal").submit(function(event) {
+        if ($("#assuntoAprender").val() == "" || $("#assuntoAprender").val().trim() == "") {
+            $(".alert").show();
+        } else {
+            $(".alert").hide();
+
+            var assunto = $("#assuntoAprender").val();
+            var checkbox = $('input[name=assuntos]');
+            var checkedValue = checkbox.filter(':checked').val();
+
+            if (checkedValue == "sim") {
+                $(".table-bordered").append(
+                    "<tr class='success'>" +
+                    "<td>" + assunto + "</td>" +
+                    "<td>" +
+                    " <div class='btn-group'><a value=" + "'" + assunto + "'" + " class='btn btn-success disabled mover'><i class='icon-thumbs-up icon-white'></i> Já Aprendi</a>" +
+                    " <a href='javascript:void(0);' class='btn btn-danger remove'><i class='icon-trash icon-white'></i> Remover</a></div>" +
+                    "</td>" +
+                    "</td>" +
+                    "</tr>");
+            } else {
+                $(".table-bordered").append(
+                    "<tr class='error'>" +
+                    "<td>" + assunto + "</td>" +
+                    "<td>" +
+                    " <div class='btn-group'><a href='javascript:void(0);' value=" + "'" + assunto + "'" + " class='btn btn-success mover'><i class='icon-thumbs-up icon-white'></i> Já Aprendi</a>" +
+                    " <a href='javascript:void(0);' class='btn btn-danger remove'><i class='icon-trash icon-white'></i> Remover</a></div>" +
+                    "</td>" +
+                    "</tr>");
+            }
+        }
+        event.preventDefault();
+    });
+    /*
+     * Remover Elemento
+     */
+    $(document).on('click', 'a.remove', function() {
+        $(this).closest('tr').remove();
+    });
+    /*
+        Mover Elemento
+    */
+    $(document).on('click', 'a.mover', function() {
+    	$(this).parent().parent().parent().removeClass("error").addClass("success");
+        $(this).addClass('disabled');
+    });
+});
